7d0a95fd86e14b02063f2f1aac6a4cdd4824df99
Kevin Lam <Kevin@AdminisatorsMBP.attlocal.net>
2023-04-18 19:34:55 -0400

    final

--- 8/script.js
+++ 8/script.js
@@ -1,8 +1,8 @@
-let str = " Hello World   ";
-let result = "";
-
-for (let i = 0; i < str.length; i++) {
-  if (str[i] !== " ") {
-    result += str[i];
-  }
+// Split method splits the string into an array of words
+// Join method joins the words back together with no space between them
+function remSpace(str) {
+  return str.split(" ").join("");
 }
+let string = "     Hello,        World!        ";
+let newString = remSpace(string);
+console.log(newString);
